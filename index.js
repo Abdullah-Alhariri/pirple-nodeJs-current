7f1b118dbf8a9b9baeedeedfaeba67fe3c5d5bf3
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2021-04-20 09:10:32 +0200

    VM added

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
  */
 
 // Dependencies
-var server = require("./lib/server");
-var workers = require("./lib/workers");
-var cli = require("./lib/cli");
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
 
 // Init function
-app.init = function (callback) {
+app.init = function(callback){
+
   // Start the server
   server.init();
 
@@ -20,16 +21,18 @@ app.init = function (callback) {
   workers.init();
 
   // Start the CLI, but make sure it starts last
-  setTimeout(function () {
+  setTimeout(function(){
     cli.init();
     callback();
-  }, 50);
+  },50);
+
 };
 
 // Self invoking only if required directly
-if (require.main === module) {
-  app.init(function () {});
+if(require.main === module){
+  app.init(function(){});
 }
+
 
 // Export the app
 module.exports = app;
